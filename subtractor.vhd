3b10fc619d6ba128790ce46dd85c0406a5b65cf1
davicorreiajr <davicorreiajr@gmail.com>
2018-07-28 18:34:35 -0300

    mult adder and sub

---
+++ subtractor.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all; 
+use IEEE.NUMERIC_STD_UNSIGNED.all;
+
+entity subtractor is
+  port(
+    a, b: in  STD_LOGIC_VECTOR(31 downto 0);
+    y:    out STD_LOGIC_VECTOR(31 downto 0)
+  );
+end;
+
+architecture behave of subtractor is
+begin
+  y <= a + not b + '1';
+end;
